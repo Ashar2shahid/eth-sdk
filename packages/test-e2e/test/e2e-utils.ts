6b1eb76349a195c4fbcf752b929c84ae5e116a22
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-05-28 22:20:43 +0200

    fix tests

--- packages/test-e2e/test/e2e-utils.ts
+++ packages/test-e2e/test/e2e-utils.ts
@@ -2,11 +2,11 @@ import { execSync } from 'child_process'
 import { rmSync } from 'fs-extra'
 import { resolve } from 'path'
 
-const THROWAWAY_MAINNET_ENDPOINT = 'https://mainnet.infura.io/v3/0993a4f4500c4fff88649d28b331898c'
+const THROWAWAY_MAINNET_ENDPOINT = 'https://rpc.mevblocker.io'
 
 export const env = {
   E2E_RPC: process.env.E2E_RPC || THROWAWAY_MAINNET_ENDPOINT,
-  SEPOLIA_RPC: THROWAWAY_MAINNET_ENDPOINT.replace('mainnet', 'sepolia'),
+  SEPOLIA_RPC: 'https://eth-sepolia.api.onfinality.io/public',
 }
 
 export function rmrf(path: string) {
